2c1977f4ea3e97c9a6b76665a78a60572d45905a
Ubuntu <ubuntu@ip-172-31-85-19.ec2.internal>
2019-10-23 16:49:35 +0000

    completed sql database, routes-planning uploaded, basic skeleton for creating work profiles added

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
-         ___        ______     ____ _                 _  ___  
-        / \ \      / / ___|   / ___| | ___  _   _  __| |/ _ \ 
-       / _ \ \ /\ / /\___ \  | |   | |/ _ \| | | |/ _` | (_) |
-      / ___ \ V  V /  ___) | | |___| | (_) | |_| | (_| |\__, |
-     /_/   \_\_/\_/  |____/   \____|_|\___/ \__,_|\__,_|  /_/ 
- ----------------------------------------------------------------- 
+Resoures:
 
+list of job categories for work profile
+https://www.recruiter.com/careers/
 
-Hi there! Welcome to AWS Cloud9!
-
-To get started, create some files, play with the terminal,
-or visit https://docs.aws.amazon.com/console/cloud9/ for our documentation.
-
-Happy coding!
+list of job levels for work profile
+https://bizfluent.com/info-8386051-five-types-jobs-levels.html
--- app.py
+++ app.py
@@ -4,9 +4,15 @@ import pymysql
 
 app = Flask(__name__)
 
+##job categories is to the credit of recruiter.com, refer to readme file for more details
+job_categories = ["Agriculture, Food and Natural Resources", "Architecture and Construction", "Arts, Audio/Video Technology and Communications",  "Business Management and Administration", "Education and Training", "Finance", "Government and Public Administration", "Health Science", "Hospitality and Tourism", "Human Services", "Information Technology", "Law, Public Safety, Corrections and Security", "Manufacturing", "Marketing, Sales and Service", "Science, Technology, Engineering and Mathematics", "Transportation, Distribution and Logistics"]
+
+##referred types of job levels by bizfluent website, refer to readme file for more details
+job_levels = ["Intern", "Junior-Level", "Entry-Level", "Intermediate", "First-level Management", "Upper-Management","Top-Level Management"]
+
 @app.route('/')
 def home():
-    return render_template("index.template.html")
+    return render_template("home.html", job_categories = job_categories, job_levels = job_levels)
 
 
 if __name__ == '__main__':
